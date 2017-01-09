d34a30916126fcdcb301a180688a97a0811d5359
Michael Leow <leow@sinarproject.org>
2017-01-09 15:14:27 +0800

    Initial foundation for nodejs tap tests

--- NATIVE_ES6_TAP/libs/scrape-single-page.js
+++ NATIVE_ES6_TAP/libs/scrape-single-page.js
@@ -4,6 +4,6 @@ module.exports = function(url) {
 
     return {
         "content": "There is some content",
-        "status": "OK"
+        "status": "Ok"
     }
 }
--- NATIVE_ES6_TAP/test/find-latest/scrape-single-page.test.js
+++ NATIVE_ES6_TAP/test/find-latest/scrape-single-page.test.js
@@ -1,14 +1,14 @@
 "use strict";
 const util = require("util")
-const tap = require("tap")
+const mytest = require("tap")
 
 const ssp = require("../../libs/scrape-single-page")
 
 // tap.pass("This is fine! mY first test!!!")
 
 const myresp = ssp("mbpj.gov.my")
-console.log(util.inspect(myresp))
-tap.equal(myresp.status, "OK")
+// DEBUG: console.log(util.inspect(myresp))
+mytest.equal(myresp.status, "OK")
 
 
-tap.end()
+// mytest.end()
